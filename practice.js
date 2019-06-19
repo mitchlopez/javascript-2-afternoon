bae8c065bcda45348c6f975487be503eb26fa907
Mitchell Lopez <mitchellslopez@gmail.com>
2019-06-19 15:41:20 -0500

    completed the assignment other than the additional practice

--- practice.js
+++ practice.js
@@ -14,7 +14,12 @@
   Then alert your name using dot notation.
 */
 
-//Code here
+let me = {
+  name: 'Mitchell',
+  age: 22
+}
+
+// alert(name.age)
 
 ////////// PROBLEM 2 //////////
 
@@ -23,21 +28,30 @@
   Have the values to those keys be strings that are equal to your favorite thing in that category.
 */
 
-//Code here
+let favoriteThings = {
+  band: 'Broken Bells',
+  food: 'grilled chicken',
+  person:'parents',
+  book: 'Harry Potter',
+  movie: 'Avatar',
+  holiday: 'Christmas'
+}
+
 
 /*
   After you've made your object, use bracket or dot notation to add another key named 'car' with the value being your favorite car
   and then another key named 'brand' with the value being your favorite brand.
 */
 
-//Code here
+favoriteThings.car = 'Lexus LFA';
+favoriteThings.brand = 'Nike'
 
 /*
   Now use bracket or dot notation to change the value of the food key in your favoriteThings object to be 'Chicken Nuggets'
   and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 */
 
-//Code here
+favoriteThings.food = 'Chicken Nuggets'
 
 ////////// PROBLEM 3 //////////
 
@@ -48,14 +62,17 @@
   Set the value of that key to 'chapstick'.
   Using dot notation, add another key (or property) to your backPack object that is named color, with the value being the color of your backpack.
 */
+let backPack = {
 
-//Code here
-
+}
+let item = 'firstPocket';
+backPack[item] = 'chapstick';
+backPack.color = 'black';
 /*
   After you do the above, alert your entire backPack object.
 */
 
-//Code here
+//alert(backPack)
 
 /*
 You probably noticed that it just alerted [object Object].
@@ -63,7 +80,7 @@ Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console.
 */
 
-//Code here
+console.log(backPack);
 
 ////////// PROBLEM 4 //////////
 
@@ -84,7 +101,8 @@ var user2 = {
   Make that change without modifying the original object code above.
 */
 
-//Code Here
+user2.name = 'Bryan G. Smith';
+user2.email = 'bryan.smith@devmounta.in';
 
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
 ////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
